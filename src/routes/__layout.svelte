31711b01f0c1a63b5471f2660103f2b4a91332a7
Clément Renou <clement.renou@mmibordeaux.com>
2021-10-05 16:11:53 +0200

    :tada: init

---
+++ src/routes/__layout.svelte
@@ -0,0 +1,14 @@
+<script>
+  import "../global.css"
+</script>
+
+<header>Header</header>
+
+<main>
+  <slot />
+</main>
+
+<footer>Footer</footer>
+
+<style>
+</style>
---
+++ src/routes/index.svelte
@@ -0,0 +1,8 @@
+<script>
+  import Test from "$lib/Test.svelte"
+</script>
+
+<h1>Col'Art</h1>
+<Test />
+
+<style></style>
