67b6a32b1790b8a3482f2bb0689ebbd2694d11d5
Clément Renou <clement.renou@mmibordeaux.com>
2021-10-06 15:15:44 +0200

    :sparkles: burger icon

--- src/lib/Layout/Header.svelte
+++ src/lib/Layout/Header.svelte
@@ -3,8 +3,8 @@
 
 <svg
   width="64"
-  height="16"
-  viewBox="0 0 64 18"
+  height="20"
+  viewBox="0 -2 64 20"
   fill="none"
   xmlns="http://www.w3.org/2000/svg"
   stroke="#323232"
@@ -14,4 +14,10 @@
   <line x1="32" y1="16" x2="64" y2="16" />
 </svg>
 
-<style></style>
+<style>
+  svg {
+    position: absolute;
+    top: 4rem;
+    right: 8rem;
+  }
+</style>
